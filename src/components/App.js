785710ca74dff0c60c56f9234c47fbd5fecda338
Shashi Raj <84275426+Shashi242@users.noreply.github.com>
2023-03-15 10:22:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,12 @@
 import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
-function SayHello(){
-return (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
-}
-
 function App() {
+  
+  function SayHello(){
+     return (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>)
+  }
+  
   return (
     <div id="main">
       // Do not alter the main div
